6f94f54d74adc78add64ffe3c65b426aa570f421
Jens Krumsieck <jenso666@live.de>
2021-05-10 20:21:36 +0200

    add ray struct

---
+++ RayTracer.Tests/RayTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RayTracer.Engine;
+
+namespace RayTracer.Tests
+{
+    [TestClass]
+    public class RayTest
+    {
+        [TestMethod]
+        public void CreateRay()
+        {
+            var ray = new Ray(Vector3.Point(1, 2, 3), Vector3.Vector(4, 5, 6));
+            Assert.AreEqual(Vector3.Point(1, 2, 3), ray.Origin);
+            Assert.AreEqual(Vector3.Vector(4, 5, 6), ray.Direction);
+        }
+    }
+}
---
+++ RayTracer/Engine/Ray.cs
@@ -0,0 +1,14 @@
+﻿namespace RayTracer.Engine
+{
+    public struct Ray
+    {
+        public Vector3 Origin;
+        public Vector3 Direction;
+
+        public Ray(Vector3 origin, Vector3 direction)
+        {
+            Origin = origin;
+            Direction = direction;
+        }
+    }
+}
