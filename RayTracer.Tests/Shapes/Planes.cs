940cc8b6558fa90d7177e0166c1f637e1d71ecea
Jens Krumsieck <jenso666@live.de>
2021-05-16 22:14:03 +0200

    added plane

---
+++ RayTracer.Tests/Shapes/Planes.cs
@@ -0,0 +1,63 @@
+﻿using System.Numerics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static RayTracer.Extension.VectorExtension;
+using Plane = RayTracer.Shapes.Plane;
+
+namespace RayTracer.Tests.Shapes
+{
+    [TestClass]
+    public class Planes
+    {
+        [TestMethod]
+        public void PlaneNormalIsUpVector()
+        {
+            var p = new Plane();
+            var n1 = p.LocalNormal(Point(0f, 0f, 0f));
+            var n2 = p.LocalNormal(Point(202f, 110f, 0f));
+            var n3 = p.LocalNormal(Point(-202f, 1e10f, 99f));
+            Assert.That.VectorsAreEqual(n1, Vector4.UnitY);
+            Assert.That.VectorsAreEqual(n3, Vector4.UnitY);
+            Assert.That.VectorsAreEqual(n2, Vector4.UnitY);
+        }
+
+        [TestMethod]
+        public void IntersectRayParallel()
+        {
+            var p = new Plane();
+            var r = new Ray(0f, 10f, 0f, 0f, 0f, 1f);
+            var xs = p.IntersectLocal(r);
+            Assert.AreEqual(xs.Length, 0);
+        }
+
+        [TestMethod]
+        public void IntersectCoplanarRay()
+        {
+            var p = new Plane();
+            var r = new Ray(0f, 0f, 0f, 0f, 0f, 1f);
+            var xs = p.IntersectLocal(r);
+            Assert.AreEqual(xs.Length, 0);
+        }
+
+        [TestMethod]
+        public void IntersectPlaneFromAbove()
+        {
+            var p = new Plane();
+            var r = new Ray(0f, 1f, 0f, 0f, -1f, 0f);
+            var xs = p.IntersectLocal(r);
+            Assert.AreEqual(xs.Length,1);
+            Assert.AreEqual(xs[0].Distance, 1f);
+            Assert.AreEqual(xs[0].Object, p);
+        }
+
+        [TestMethod]
+        public void IntersectPlaneFromBelow()
+        {
+            var p = new Plane();
+            var r = new Ray(0f, -1f, 0f, 0f, 1f, 0f);
+            var xs = p.IntersectLocal(r);
+            Assert.AreEqual(xs.Length, 1);
+            Assert.AreEqual(xs[0].Distance, 1f);
+            Assert.AreEqual(xs[0].Object, p);
+        }
+    }
+}
---
+++ RayTracer/Shapes/Plane.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+using static RayTracer.Extension.VectorExtension;
+
+namespace RayTracer.Shapes
+{
+    public sealed class Plane : Entity
+    {
+        public Plane(Transform transform) : base(transform) { }
+        public Plane() { }
+
+        public override Intersection[] IntersectLocal(in Ray r)
+        {
+            if (MathF.Abs(r.Direction.Y) < Constants.Epsilon) return Array.Empty<Intersection>();
+            var t = -r.Origin.Y / r.Direction.Y;
+            return new[] { new Intersection(t, this) };
+        }
+
+        public override Vector4 LocalNormal(in Vector4 at) => Direction(0f, 1f, 0f);
+    }
+}
